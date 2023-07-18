e0a6855d1ef6ba87dafd39fd05c6b13ee4c93a5a
RistoFlink <kenssi@gmail.com>
2023-07-18 13:04:43 +0300

    more functionality

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,9 +1,21 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+        let b = b.unwrap();
+        let c = b as char; //as transforms to other primitive types
+
+        //check if the input is a control character (ASCII 0-31 & 127) and don't print it
+        //otherwise, print the character
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?}, ({})\r", b, c);
+        }
+        //stop the program if user inputs q
         if c == 'q' {
             break;
         }
