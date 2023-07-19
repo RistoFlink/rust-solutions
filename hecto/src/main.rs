50c32c16273b47134ad9d95c90383017471ffd02
RistoFlink <kenssi@gmail.com>
2023-07-19 14:58:16 +0300

    add status bar and ability to move in document

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,14 +1,17 @@
 //#![warn(clippy::all, clippy::pedantic)]
+mod document;
 mod editor;
+mod row;
 mod terminal;
 // fn to_ctrl_byte(c: char) -> u8 {
 //     //perform a bitwise AND-operation to check if control is pressed
 //     let byte = c as u8;
 //     byte & 0b0001_1111
 // }
-
+pub use document::Document;
 use editor::Editor;
 pub use editor::Position;
+pub use row::Row;
 pub use terminal::Terminal;
 
 fn main() {
