5d3aaa8576bde2c95b942c0288d5f6e525184ed9
RistoFlink <kenssi@gmail.com>
2023-08-17 15:50:58 +0300

    Add ability to return HTML

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     net::{TcpListener, TcpStream},
     io::{prelude::*, BufReader}
 };
@@ -21,5 +22,12 @@ fn handle_connection(mut stream: TcpStream){
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("Request: {:#?}", http_request);
-}
+    //println!("Request: {:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}   
