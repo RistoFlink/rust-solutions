5f2a39ea6a6dbecfd51de7f52238f1c38395b137
RistoFlink <kenssi@gmail.com>
2023-08-18 20:08:50 +0300

    Implement shutdown and cleanup

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,8 +1,9 @@
 use std::{
-    thread,
     fs,
+    io::prelude::*,
     net::{TcpListener, TcpStream},
-    io::{prelude::*, BufReader}, time::Duration
+    thread,
+    time::Duration,
 };
 
 use web_server::ThreadPool;
@@ -10,42 +11,48 @@ use web_server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        let pool = ThreadPool::new(4);
-        
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
+    let pool = ThreadPool::new(4);
 
-            pool.execute(|| {
-                handle_connection(stream);
-            });
-        //the following would create an unlimited number of threads and potentially overload the
-        //system
-        //thread::spawn(|| {
-        //  handle_connection(stream);
-        //  });
-        }
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+    //the following would create an unlimited number of threads and potentially overload the
+    //system
+    //thread::spawn(|| {
+    //  handle_connection(stream);
+    //  });
+    println!("Shutting down.");
 }
-fn handle_connection(mut stream: TcpStream){
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{contents}");
+    let response = format!(
+        "{}\r\nContent-length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
-
