1933448548b2c9de54f64e4f6edc11eda8136392
RistoFlink <kenssi@gmail.com>
2024-01-16 08:51:30 +0200

    Update get_args with the --help output

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -17,7 +17,50 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Risto Flink <risto.flink@pm.me>")
         .about("Rust wc")
-        // TODO
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input files")
+                .required(true)
+                .min_values(1)
+                .default_value("-"),
+            )
+        .arg(
+            Arg::with_name("lines")
+                .value_name("LINES")
+                .help("Show line count")
+                .short("l")
+                .long("--lines")
+                .required(false)
+                .default_value("true"),
+            )
+        .arg(
+            Arg::with_name("words")
+                .value_name("WORDS")
+                .help("Show word count")
+                .short("w")
+                .long("--words")
+                .required(false)
+                .default_value("true"),
+            )
+        .arg(
+            Arg::with_name("bytes")
+                .value_name("BYTES")
+                .help("Show byte count")
+                .short("c")
+                .long("--bytes")
+                .required(false)
+                .default_value("true"),
+            )
+        .arg(
+            Arg::with_name("chars")
+                .value_name("CHARS")
+                .help("Show character count")
+                .short("m")
+                .long("--chars")
+                .required(false)
+                .default_value("false"),
+            )
         .get_matches();
 
     Ok(Config {
