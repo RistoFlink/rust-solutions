ba0b1795ae0f8d761a40884faf8562553110d394
RistoFlink <kenssi@gmail.com>
2025-06-21 23:25:48 +0300

    Add IDT loading and invoking the breakpoint handler

--- rust-os/src/main.rs
+++ rust-os/src/main.rs
@@ -11,31 +11,17 @@ use rust_os::println;
 
 #[no_mangle] //don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    //this is the entry point since the linker loosk for a function named _start by default
-    /*     let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap(); */
-
     println!("Hello, world{}", "!");
     //panic!("Generic panic message");
+    rust_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     // IDE complains about this missing but it still runs..
     test_main();
+    println!("All good!");
     loop {} //indefinite loop
 }
 
