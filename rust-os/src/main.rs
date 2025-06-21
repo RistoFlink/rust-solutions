3abcf7a44ea345fe5788fa3fa69947272eaf41ab
RistoFlink <kenssi@gmail.com>
2025-06-21 20:05:01 +0300

    Add automatic printing insertion

--- rust-os/src/main.rs
+++ rust-os/src/main.rs
@@ -33,10 +33,10 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} test(s)", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     // new
     exit_qemu(QemuExitCode::Success);
@@ -44,9 +44,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -93,6 +91,21 @@ pub extern "C" fn _start() -> ! {
     // IDE complains about this missing but it still runs..
     test_main();
     loop {} //indefinite loop
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
 
 // to run it in the virtual machine: qemu-system-x86_64 -drive format=raw,file=target/os/debug/bootimage-rust-os.bin
