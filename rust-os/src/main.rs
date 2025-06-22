117f0f0e9b924805cf9541af35fc86d5df1e934a
RistoFlink <kenssi@gmail.com>
2025-06-22 14:28:37 +0300

    Create a TSS and GDT

--- rust-os/src/main.rs
+++ rust-os/src/main.rs
@@ -9,6 +9,8 @@
 use core::panic::PanicInfo;
 use rust_os::println;
 
+pub mod gdt;
+
 #[no_mangle] //don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     println!("Hello, world{}", "!");
@@ -19,9 +21,16 @@ pub extern "C" fn _start() -> ! {
     // x86_64::instructions::interrupts::int3();
 
     // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u8) = 42;
-    };
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // };
+
+    fn stack_overflow() {
+        stack_overflow(); // push the return address for each recursion
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
 
     #[cfg(test)]
     // IDE complains about this missing but it still runs..
