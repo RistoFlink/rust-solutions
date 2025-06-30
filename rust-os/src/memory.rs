c08c3416b139ac3d98d50fc97a02b717e5642c4d
RistoFlink <kenssi@gmail.com>
2025-06-30 22:51:28 +0300

    Add a method that converts the memory map into an iterator of usable frames

--- rust-os/src/memory.rs
+++ rust-os/src/memory.rs
@@ -90,6 +90,25 @@ impl BootInfoFrameAllocator {
     }
 }
 
+use bootloader::bootinfo::MemoryRegionType;
+
+impl BootInfoFrameAllocator {
+    /// Returns an iterator over the usable frames specified in the memory map
+    fn usable_frame(&self) -> impl Iterator<Item = PhysFrame> {
+        // get usable regions from memory_map
+        let regions = self.memory_map.iter();
+        let usable_regions = regions
+            .filter(|r| r.region_type == MemoryRegionType::Usable);
+        // map each region to its address range
+        let addr_ranges = usable_regions
+            .map(|r| r.range.start_addr()..r.range.end_addr());
+        // transform to an iterator of frame start addresses
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        // create PhysFrame types from the start addresses
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
 // Translates the given virtual address to the mapped physical address, or
 // `None` if the address is not mapped.
 //
