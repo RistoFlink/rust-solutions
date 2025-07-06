d210d5a8f05f708187738410ac04b947bed88682
RistoFlink <kenssi@gmail.com>
2025-07-06 22:17:36 +0300

    Implement a bump allocator

--- rust-os/src/allocator.rs
+++ rust-os/src/allocator.rs
@@ -5,6 +5,8 @@ use linked_list_allocator::LockedHeap;
 pub struct Dummy;
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+pub mod bump;
+mod bump;
 
 unsafe impl GlobalAlloc for Dummy {
     unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
