589c5c81f474e778dc4fbf839a22e88bbf5b2ec7
RistoFlink <kenssi@gmail.com>
2025-07-08 21:47:44 +0300

    Update GlobalAlloc and create function for address alignment

--- rust-os/src/allocator.rs
+++ rust-os/src/allocator.rs
@@ -69,5 +69,12 @@ pub fn init_heat(
     Ok(())
 }
 
+/// Align the given address `addr` upwards to alignment `align`.
+///
+/// Requires that `align` is a power of two.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
+
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
--- rust-os/src/allocator/bump.rs
+++ rust-os/src/allocator/bump.rs
@@ -28,17 +28,34 @@ impl BumpAllocator {
 }
 
 use alloc::alloc::{GlobalAlloc, Layout};
+use core::ptr;
+use x86_64::align_up;
+use super::{align_up, Locked};
 
-unsafe impl GlobalAlloc for spin::Mutex<BumpAllocator> {
+unsafe impl GlobalAlloc for Locked<BumpAllocator> {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        // TODO alignment and bounds check
-        let alloc_start = self.next;
-        self.next = alloc_start + layout.size();
-        self.allocations += 1;
-        alloc_start as *mut u8
+        let mut bump = self.lock(); // get a mut reference
+        let alloc_start = align_up(bump.next, layout.align());
+        let alloc_end = match alloc_start.checked_add(layout.size()) {
+            Some(end) => end,
+            None => return ptr::null_mut(),
+        };
+
+        if alloc_end > bump.heap_end {
+            ptr::null_mut() // out of memory
+        } else {
+            bump.next = alloc_end;
+            bump.allocations += 1;
+            alloc_start as *mut u8
+        }
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        todo!();
+        let mut bump = self.lock(); // get mut reference
+
+        bump.allocations -= 1;
+        if bump.allocations == 0 {
+            bump.next = bump.heap_start;
+        }
     }
 }
