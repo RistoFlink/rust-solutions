fa2d24c9a3491e13e72e1b5e9f011ed065ee5cd2
RistoFlink <kenssi@gmail.com>
2025-07-14 18:37:19 +0300

    Implement the GlobalAlloc trait

--- rust-os/src/allocator/fixed_size_block.rs
+++ rust-os/src/allocator/fixed_size_block.rs
@@ -1,5 +1,6 @@
-use alloc::alloc::Layout;
-use core::ptr;
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::{mem, ptr, ptr::NonNull};
+use super::Locked;
 
 /// The block sizes to use.
 ///
@@ -49,6 +50,57 @@ impl FixedSizeBlockAllocator {
         match self.fallback_allocator.allocate_first_fit(layout) {
             Ok(ptr) => ptr.as_ptr(),
             Err(_) => ptr::null_mut(),
+        }
+    }
+}
+
+unsafe impl GlobalAlloc for Locked<FixedSizeBlockAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut allocator = self.lock();
+        match list_index(&layout) {
+            Some(index) => {
+                match allocator.list_heads[index].take() {
+                    Some(node) => {
+                        allocator.list_heads[index] = node.next.take();
+                        node as *mut ListNode as *mut u8
+                    }
+                    None => {
+                        // no blocks exist in list -> allocate new block
+                        let block_size = BLOCK_SIZES[index];
+                        // only works if block sizes are a power of 2
+                        let block_align = block_size;
+                        let layout = Layout::from_size_align(block_size, block_align)
+                            .unwrap();
+                        allocator.fallback_alloc(layout)
+                    }
+                }
+            }
+            None => allocator.fallback_alloc(layout),
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        let mut allocator = self.lock();
+        match list_index(&layout) {
+            Some(index) => {
+                let new_node = ListNode {
+                    next: allocator.list_heads[index].take(),
+                };
+                // very that block has size and alignment required for storing node
+                assert!(mem::size_of::<ListNode>() <= BLOCK_SIZES[index]);
+                assert!(mem::align_of::<ListNode>() <= BLOCK_SIZES[index]);
+                let new_node_ptr = ptr as *mut ListNode;
+                unsafe {
+                    new_node_ptr.write(new_node);
+                    allocator.list_heads[index] = Some(&mut *new_node_ptr);
+                }
+            }
+            None => {
+                let ptr = NonNull::new(ptr).unwrap();
+                unsafe {
+                    allocator.fallback_allocator.deallocate(ptr, layout);
+                }
+            }
         }
     }
 }
